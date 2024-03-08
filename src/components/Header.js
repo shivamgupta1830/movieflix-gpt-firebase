4f62eb1d0d4278fa37c5a787a2455583b925bed7
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-03-08 23:29:35 +0530

    basic UI improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from "../assets/netflix-logo.png";
 const Header = () => {
   return (
     <div className=" w-full absolute bg-gradient-to-b from-black z-20">
-      <img src={logo} alt="logo" className="w-[17%] px-4 pt-2"></img>
+      <img src={logo} alt="logo" className="w-[16%] px-4 pt-2"></img>
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -14,39 +14,42 @@ const Login = () => {
       <div className="relative">
         <img src={background} alt="background"></img>
       </div>
-      <form className=" w-[28%] text-center bg-black bg-opacity-90 absolute top-20 mx-auto right-0 left-0 px-12 py-16  flex flex-col justify-start items-start rounded-sm text-white">
+      <form className=" w-[28%] text-center bg-black bg-opacity-90 absolute top-20 mx-auto right-0 left-0 px-12 py-10  flex flex-col justify-start items-start rounded-md text-white" onSubmit={(e)=>{e.preventDefault()}}>
         <h2 className="text-white font-bold text-3xl my-3">
-          {isSignIn ? "Sign in" : "Log in"}
+          {isSignIn ? "Sign In" : "Log In"}
         </h2>
 
         {isSignIn && (
           <input
             type="text"
             placeholder="Full Name"
-            className="p-2 my-3 w-full bg-slate-800 rounded-sm"
+            className="p-3 my-3 w-full bg-slate-800 rounded-md"
           />
         )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-2 my-3 w-full bg-slate-800 rounded-sm"
+          className="p-3 my-3 w-full bg-slate-800 rounded-md"
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-2 my-3 bg-slate-800 w-full rounded-sm"
+          className="p-3 my-3 bg-slate-800 w-full rounded-md"
         />
-        <button className="bg-red-700 w-full py-2 px-3 my-3 rounded-sm hover:bg-red-600 font-medium">
-          {isSignIn ? "Sign in" : "Log in"}
+        <button
+          className="bg-red-700 w-full p-3 my-3 rounded-md hover:bg-red-600 font-medium"
+          onClick={() => {}}
+        >
+          {isSignIn ? "Sign In" : "Log In"}
         </button>
-        <p className="text-white p-2 my-3 ">
+        <p className="text-white p-3 my-3 ">
           {isSignIn ? "Already registered? " : "Not registerd? "}
 
           <span
             className="cursor-pointer hover:underline"
             onClick={signInHandle}
           >
-            {isSignIn ? "Log in" : "Sign in Now"}
+            {isSignIn ? "Log In" : "Sign In Now"}
           </span>
         </p>
       </form>
