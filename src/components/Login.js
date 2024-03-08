13a3dd402b800fd0977242f99da1f705801c616e
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-03-08 22:41:47 +0530

    log in & sign in toggle

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import background from "../assets/netflix-background.jpg";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const signInHandle = () => {
+    setIsSignIn(!isSignIn);
+  };
   return (
     <div>
       <Header />
       <div className="relative">
         <img src={background} alt="background"></img>
       </div>
-      <form className=" w-[30%] h-auto text-center bg-black opacity-95 absolute top-28 mx-auto right-0 left-0 px-12 py-20 flex flex-col justify-start items-start rounded-sm">
-        <h2 className="text-white font-medium text-lg my-2">Sign In</h2>
+      <form className=" w-[28%] text-center bg-black bg-opacity-90 absolute top-20 mx-auto right-0 left-0 px-12 py-16  flex flex-col justify-start items-start rounded-sm text-white">
+        <h2 className="text-white font-bold text-3xl my-3">
+          {isSignIn ? "Sign in" : "Log in"}
+        </h2>
+
+        {isSignIn && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-3 w-full bg-slate-800 rounded-sm"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-2 my-2 w-11/12 bg-slate-800 rounded-sm"
+          className="p-2 my-3 w-full bg-slate-800 rounded-sm"
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-2 my-2 bg-slate-800 w-11/12 rounded-sm"
+          className="p-2 my-3 bg-slate-800 w-full rounded-sm"
         />
-        <button className="bg-red-700 py-2 px-3 my-2 rounded-sm hover:bg-red-600 font-medium">
-          Sign In
+        <button className="bg-red-700 w-full py-2 px-3 my-3 rounded-sm hover:bg-red-600 font-medium">
+          {isSignIn ? "Sign in" : "Log in"}
         </button>
+        <p className="text-white p-2 my-3 ">
+          {isSignIn ? "Already registered? " : "Not registerd? "}
+
+          <span
+            className="cursor-pointer hover:underline"
+            onClick={signInHandle}
+          >
+            {isSignIn ? "Log in" : "Sign in Now"}
+          </span>
+        </p>
       </form>
     </div>
   );
