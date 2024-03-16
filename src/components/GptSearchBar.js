5528436da16be57cc6d87772bfac5a4ca24f0307
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-03-16 15:58:02 +0530

    UI improvements

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const GptSearchBar = () => {
   return (
-    <div className="mx-auto w-[60%] pt-[6%]">
+    <div className="mx-auto w-[45%] pt-[6%]">
       <form
-        className="bg-black p-4 flex gap-2 rounded-md"
+        className="bg-black p-4 flex gap-2 rounded-md bg-opacity-85"
         onClick={(e) => e.preventDefault()}
       >
         <input
-          className="w-3/4  py-2 px-2 text-semibold text-lg text-white bg-slate-800 rounded-l-md"
+          className="w-[80%]  p-2 text-semibold  text-white bg-slate-800 rounded-l-md"
           type="text"
           placeholder="What would you like to watch today?"
         />
-        <button className="bg-red-700 hover:bg-red-600 w-1/4  py-2 px-2 text-white text-semibold text-lg rounded-r-md">
+        <button className="bg-red-700 hover:bg-red-600 w-[20%]  p-2 text-white text-semibold rounded-r-md">
           Search
         </button>
       </form>
