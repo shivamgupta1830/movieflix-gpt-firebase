2c820a9a125b2030f9278e88537525762a71fa9c
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-03-20 16:45:25 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,12 +2,10 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzk5MWYwYWNhYzUxZWY1YTlmOGMzM2RjYjJjMjk0OSIsInN1YiI6IjY0NmM1MmQ3YzM1MTRjMmIwODUxY2Q0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SXR5cXinm_1CnLiPncgZwibo0YH8laUFRDfm_XSgRDQ",
+    Authorization: "Bearer",
   },
 };
 
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-export const OPENAI_API_KEY =
-  "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";
+export const OPENAI_API_KEY = "";
